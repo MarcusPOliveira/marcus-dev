4c13475c746d3816915ab73b60c07b51c473be58
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2026-02-25 12:13:32 -0300

    style(button): update hover background color for button

--- src/components/knowledge/known_tech.tsx
+++ src/components/knowledge/known_tech.tsx
@@ -8,11 +8,11 @@ interface KnownTechProps {
 
 export const KnownTech = ({ tech }: KnownTechProps) => {
   return (
-    <div className="group relative flex items-center gap-4 rounded-lg bg-gray-600/20 p-6 text-gray-500 transition-all hover:bg-gray-600/30 hover:text-orange-600 hover:shadow-lg hover:shadow-orange-600/10">
-      <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-gray-600/30 text-2xl transition-all group-hover:bg-gray-600/50 group-hover:scale-110">
+    <div className="group flex items-center gap-4 rounded-lg bg-gray-600/20 p-6 text-gray-500 transition-all duration-300 hover:bg-gray-600/30 hover:text-orange-600 hover:shadow-[6px_4px_20px_rgba(234,88,12,0.15)]">
+      <div className="flex items-center justify-center w-12 h-12 text-2xl transition-colors duration-300 rounded-lg bg-gray-600/30 group-hover:bg-gray-600/50 [&>div]:transition-transform [&>div]:duration-300 group-hover:[&>div]:scale-110">
         <CmsIcon icon={tech.iconSvg} />
       </div>
-      <p className="flex-1 font-medium text-lg">{tech.name}</p>
+      <p className="flex-1 text-lg font-medium">{tech.name}</p>
     </div>
   )
 }
